93f51550febd28aa20362859a0dd2f144ea9f2f4
Trevor Mugo <trevor@qwikpace.com>
2024-05-24 15:45:59 +0300

    feat: destinationuser

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -41,6 +41,7 @@ const LinkItems: Array<LinkItemProps> = [
   { name: 'Dashboard', icon: FaHome, url: '#' },
   { name: 'Owners', icon: FaUser, url: 'create-owner' },
   { name: 'Regional Admins', icon: FaUser, url: 'create-regional-admin' },
+  { name: 'Destination User', icon: FaUser, url: 'create-destination-user' },
   { name: 'Settings', icon: FaCog, url: 'settings' },
 ];
 
