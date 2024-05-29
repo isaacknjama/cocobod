df0d3d3ea5f554f5192c505cb7aabe832edcf4ac
Isaack Njama <isaacknjama@proton.me>
2024-05-29 21:31:21 +0300

    refactor: move owners table from dashboard

--- src/components/OwnerAdmin.tsx
+++ src/components/OwnerAdmin.tsx
@@ -42,6 +42,7 @@ import { apiBaseUrl } from '../core/environment';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 export const CreateNewOwner = () => {
+  const [ownerData, setOwnerData] = useState();
   const [ownerLogo, setOwnerLogo] = useState<File | null>(null);
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
@@ -55,6 +56,8 @@ export const CreateNewOwner = () => {
   const [batchCodeLength, setBatchCodeLength] = useState<number>(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [ownerDetails, setOwnerDetails] = useState<[] | null>();
+  const [expandedRow, setExpandedRow] = useState<number | null>(null);
 
   const toast = useToast();
   const navigate = useNavigate();
@@ -126,14 +129,70 @@ export const CreateNewOwner = () => {
     ],
   );
 
+  const fetchOwners = useCallback(async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(
+        `${apiBaseUrl}/api/v1/owner/list-all-owners/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok!');
+      }
+      const owners = await response.json();
+      setOwnerData(owners);
+    } catch (err) {
+      console.error('Error fetching data: ', err);
+    }
+  }, [setOwnerData]);
+
+  const fetchOwnerDetails = async (ownerId: number) => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(
+        `${apiBaseUrl}/api/v1/owner/describe-one-owner/${ownerId}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok!');
+      }
+      const owner = await response.json();
+      setOwnerDetails(owner);
+    } catch (err) {
+      console.error('Error fetching data: ', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchOwners();
+  }, [fetchOwners]);
+
+  const handleToggleRow = async (ownerId: number) => {
+    if (expandedRow === ownerId) {
+      setExpandedRow(null);
+      setOwnerDetails(null);
+    } else {
+      setExpandedRow(ownerId);
+      await fetchOwnerDetails(ownerId);
+    }
+  };
+
   const openModal = () => {
     setIsOpen(true);
   };
 
   const closeModal = useCallback(() => {
     setIsOpen(false);
-    ListAllOwners();
-  }, []);
+    fetchOwners();
+  }, [fetchOwners]);
 
   return (
     <React.Fragment>
@@ -176,6 +235,135 @@ export const CreateNewOwner = () => {
           Add Owner Admin
         </Button>
       </div>
+
+      <Box height='87.5vh' p={4}>
+        {ownerData ? (
+          <>
+            <TableContainer
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Table
+                variant='striped'
+                colorScheme='gray'
+                w={['100%', '100%', '80%', '80%']}
+                overflowX='visible'
+              >
+                <Thead>
+                  <Tr>
+                    <Th>Owner ID</Th>
+                    <Th>Owner Name</Th>
+                    <Th>Owner Code</Th>
+                    <Th>Batch Code Length</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {ownerData?.message.map((data) => (
+                    <React.Fragment key={data.owner_id}>
+                      <Tr>
+                        <Td>{data.owner_id}</Td>
+                        <Td>{data.owner_name}</Td>
+                        <Td>{data.owner_code}</Td>
+                        <Td>{data.batch_code_length}</Td>
+                        <Td>
+                          <Link
+                            onClick={() => handleToggleRow(data.owner_id)}
+                            _hover={{
+                              color: 'yellow.500',
+                            }}
+                            textDecoration='underline'
+                          >
+                            {expandedRow === data.owner_id ? (
+                              <FiChevronUp />
+                            ) : (
+                              <FiChevronDown />
+                            )}
+                          </Link>
+                        </Td>
+                      </Tr>
+                      {expandedRow === data.owner_id && (
+                        <Card w={['100%', '90%', '80%', '60%']} mt={2}>
+                          <CardHeader>
+                            <Heading size='md'>Owner Details</Heading>
+                          </CardHeader>
+                          {ownerDetails ? (
+                            <CardBody>
+                              <Flex gap={4} flexDirection='column'>
+                                <Heading size='s'>Owner Logo</Heading>
+                                <Box boxSize='sm'>
+                                  <Image
+                                    src={data.owner_logo}
+                                    objectFit='cover'
+                                    alt='Owner Logo'
+                                    boxSize={['100px', '150px']}
+                                  />
+
+                                  <Heading size='s'>
+                                    Allow Incident Tracking
+                                  </Heading>
+                                  <Text>
+                                    {data.allow_incident_tracking
+                                      ? 'True'
+                                      : 'False'}
+                                  </Text>
+                                </Box>
+                                <Box>
+                                  <Heading size='s'>Batch Code Length</Heading>
+                                  <Text>{data.batch_code_length}</Text>
+                                </Box>
+                                <Box>
+                                  <Heading size='s'>Include District</Heading>
+                                  <Text>
+                                    {data.include_district ? 'True' : 'False'}
+                                  </Text>
+                                </Box>
+                              </Flex>
+                              <Stack
+                                divider={<StackDivider />}
+                                flexDirection='row'
+                                gap={8}
+                                wrap='wrap'
+                              >
+                                <Box>
+                                  <Heading size='s'>Include Region</Heading>
+                                  <Text>
+                                    {data.include_region ? 'True' : 'False'}
+                                  </Text>
+                                </Box>
+                                <Box>
+                                  <Heading size='s'>Owner Comment</Heading>
+                                  <Text>{data.owner_comment}</Text>
+                                </Box>
+                                <Box>
+                                  <Heading size='s'>Owner Description</Heading>
+                                  <Text>{data.owner_description}</Text>
+                                </Box>
+                                <Box>
+                                  <Heading size='s'>Serialise Codes</Heading>
+                                  <Text>{data.serialise_codes}</Text>
+                                </Box>
+                              </Stack>
+                            </CardBody>
+                          ) : (
+                            <Spinner size='lg' color='green.500' />
+                          )}
+                        </Card>
+                      )}
+                    </React.Fragment>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </>
+        ) : (
+          <Flex justifyContent='center' alignItems='center' height='100%'>
+            <Spinner size='xl' color='green.500' />
+          </Flex>
+        )}
+      </Box>
 
       <Modal isOpen={isOpen} onClose={closeModal}>
         <ModalOverlay />
@@ -336,217 +524,5 @@ export const CreateNewOwner = () => {
         </ModalContent>
       </Modal>
     </React.Fragment>
-  );
-};
-
-export const ListAllOwners = () => {
-  interface Owner {
-    owner_id: number;
-    owner_name: string;
-    owner_code: string;
-    batch_code_length: number;
-    owner_logo: string;
-    allow_incident_tracking: string;
-    include_district: string;
-    include_region: string;
-    owner_comment: string;
-    owner_description: string;
-    serialise_codes: string;
-  }
-
-  interface OwnerData {
-    message: Owner[];
-  }
-
-  const [ownerData, setOwnerData] = useState<OwnerData>();
-  const [ownerDetails, setOwnerDetails] = useState<[] | null>();
-  const [expandedRow, setExpandedRow] = useState<number | null>(null);
-
-  useEffect(() => {
-    const fetchOwners = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        const response = await fetch(
-          `${apiBaseUrl}/api/v1/owner/list-all-owners/`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        );
-        if (!response.ok) {
-          throw new Error('Network response was not ok!');
-        }
-        const owners = await response.json();
-        setOwnerData(owners);
-      } catch (err) {
-        console.error('Error fetching data: ', err);
-      }
-    };
-
-    fetchOwners();
-  }, []);
-
-  const fetchOwnerDetails = async (ownerId: number) => {
-    try {
-      const token = localStorage.getItem('token');
-      const response = await fetch(
-        `${apiBaseUrl}/api/v1/owner/describe-one-owner/${ownerId}/`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      if (!response.ok) {
-        throw new Error('Network response was not ok!');
-      }
-      const owner = await response.json();
-      setOwnerDetails(owner);
-    } catch (err) {
-      console.error('Error fetching data: ', err);
-    }
-  };
-
-  const handleToggleRow = async (ownerId: number) => {
-    if (expandedRow === ownerId) {
-      setExpandedRow(null);
-      setOwnerDetails(null);
-    } else {
-      setExpandedRow(ownerId);
-      await fetchOwnerDetails(ownerId);
-    }
-  };
-
-  return (
-    <Box height='87.5vh' p={4}>
-      {/* {console.log('Owner data:', ownerData?.message)} */}
-      {ownerData ? (
-        <>
-          <TableContainer
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Table
-              variant='striped'
-              colorScheme='gray'
-              w={['100%', '100%', '80%', '80%']}
-              overflowX='visible'
-            >
-              <Thead>
-                <Tr>
-                  <Th>Owner ID</Th>
-                  <Th>Owner Name</Th>
-                  <Th>Owner Code</Th>
-                  <Th>Batch Code Length</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {ownerData?.message.map((data) => (
-                  <React.Fragment key={data.owner_id}>
-                    <Tr>
-                      <Td>{data.owner_id}</Td>
-                      <Td>{data.owner_name}</Td>
-                      <Td>{data.owner_code}</Td>
-                      <Td>{data.batch_code_length}</Td>
-                      <Td>
-                        <Link
-                          onClick={() => handleToggleRow(data.owner_id)}
-                          _hover={{
-                            color: 'yellow.500',
-                          }}
-                          textDecoration='underline'
-                        >
-                          {expandedRow === data.owner_id ? (
-                            <FiChevronUp />
-                          ) : (
-                            <FiChevronDown />
-                          )}
-                        </Link>
-                      </Td>
-                    </Tr>
-                    {expandedRow === data.owner_id && (
-                      <Card w={['100%', '90%', '80%', '60%']} mt={2}>
-                        <CardHeader>
-                          <Heading size='md'>Owner Details</Heading>
-                        </CardHeader>
-                        {ownerDetails ? (
-                          <CardBody>
-                            <Flex gap={4} flexDirection='column'>
-                              <Heading size='s'>Owner Logo</Heading>
-                              <Box boxSize='sm'>
-                                <Image
-                                  src={data.owner_logo}
-                                  objectFit='cover'
-                                  alt='Owner Logo'
-                                  boxSize={['100px', '150px']}
-                                />
-
-                                <Heading size='s'>
-                                  Allow Incident Tracking
-                                </Heading>
-                                <Text>
-                                  {data.allow_incident_tracking
-                                    ? 'True'
-                                    : 'False'}
-                                </Text>
-                              </Box>
-                              <Box>
-                                <Heading size='s'>Batch Code Length</Heading>
-                                <Text>{data.batch_code_length}</Text>
-                              </Box>
-                              <Box>
-                                <Heading size='s'>Include District</Heading>
-                                <Text>
-                                  {data.include_district ? 'True' : 'False'}
-                                </Text>
-                              </Box>
-                            </Flex>
-                            <Stack
-                              divider={<StackDivider />}
-                              flexDirection='row'
-                              gap={8}
-                              wrap='wrap'
-                            >
-                              <Box>
-                                <Heading size='s'>Include Region</Heading>
-                                <Text>
-                                  {data.include_region ? 'True' : 'False'}
-                                </Text>
-                              </Box>
-                              <Box>
-                                <Heading size='s'>Owner Comment</Heading>
-                                <Text>{data.owner_comment}</Text>
-                              </Box>
-                              <Box>
-                                <Heading size='s'>Owner Description</Heading>
-                                <Text>{data.owner_description}</Text>
-                              </Box>
-                              <Box>
-                                <Heading size='s'>Serialise Codes</Heading>
-                                <Text>{data.serialise_codes}</Text>
-                              </Box>
-                            </Stack>
-                          </CardBody>
-                        ) : (
-                          <Spinner size='lg' color='green.500' />
-                        )}
-                      </Card>
-                    )}
-                  </React.Fragment>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </>
-      ) : (
-        <Flex justifyContent='center' alignItems='center' height='100%'>
-          <Spinner size='xl' color='green.500' />
-        </Flex>
-      )}
-    </Box>
   );
 };
